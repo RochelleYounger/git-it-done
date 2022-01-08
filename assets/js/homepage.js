acd8ee65f4f8632609c4a796d7352907cf127b23
Rochelle Younger <regnuoy321@gmail.com>
2022-01-08 00:28:11 -0500

    add featured repo logic

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,7 +2,18 @@ var userformEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
 
+var buttonClickHandler = function(event) {
+    var language = event.target.getAttribute("data-language");
+    console.log(language);
+    if (language) {
+        getFeaturedRepos(language);
+
+        // clear old content
+        repoContainerEl.textContent = "";
+    }
+};
 var formSubmitHandler = function(event) {
     event.preventDefault();
 
@@ -17,6 +28,20 @@ var formSubmitHandler = function(event) {
     }
 
     console.log(event);
+};
+
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+  
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data){
+                displayRepos(data.items, language);
+            })
+        } else {
+          alert('Error: GitHub User Not Found');
+        }
+    });
 };
 
 var getUserRepos = function(user) {
@@ -92,3 +117,5 @@ var displayRepos = function(repos, searchTerm) {
 };
 
 userformEl.addEventListener("submit", formSubmitHandler);
+
+languageButtonsEl.addEventListener("click", buttonClickHandler);
